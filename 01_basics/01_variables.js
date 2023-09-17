992382c9cec1b6ca9f366e115cc1d8b5a847ac85
Ankit Kumar <116054971+itz-ankit01@users.noreply.github.com>
2023-09-17 04:30:29 +0000

    dataype

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,3 +2,20 @@ const accountId = 144553
 let accountEmail = "abc@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2  // not allowed
+accountEmail = "anc@gmail.com"
+accountPassword = "256555"
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.log(accountEmail);
+
+
+/*
+Prefer not to use var
+cuz of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all JS code as newer version
+
+// alert( 3 + 3 )  // we are using node js not browser
+
+console.log(3 + 3)
+ 
+console.log("Ankit");
+
+let name = "Ankit"
+let age = 20
+let isLoggedIn = false
+
+// Primitive DataTypes
+// Number => 2 to power 53
+// bigInt
+// string = "Ram"
+// boolean => true / false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "Hitesh");
+console.log(typeof undefined);   // undefined
+console.log(typeof age);
+console.log(typeof null);  // object
