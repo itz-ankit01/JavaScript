d27564d41e4d4c5bbd660fc53c15ac03edbb7c26
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2023-12-21 14:58:34 +0000

    project

---
+++ 07_projects/1-colorChanger/chaiaurcode.js
@@ -0,0 +1,27 @@
+const button = document.querySelectorAll('.button')
+
+const body = document.querySelector('body')
+
+// events
+button.forEach(function (btn){
+    console.log(btn);
+    btn.addEventListener('click', function(e){         // e = object of event 
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'purple'){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
