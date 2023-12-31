33f652023acd6c94aa1ff3e51f18c3cc92da69b0
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2023-12-31 11:13:42 +0000

    spotify project

---
+++ Projects/script.js
@@ -0,0 +1,12 @@
+console.log("Let's write JavaScript");
+
+async function main(){
+    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let response = await a.text();
+    console.log(response);
+}
+main()
+
+
+
+
